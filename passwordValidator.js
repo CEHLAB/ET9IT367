ae5a0116ce723a5c896d4d605388f6c30a6a9116
Chakir EL BOJADDAINI <chakirelb1@gmail.com>
2024-11-05 17:03:55 +0100

    Ajouter letter validation

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,7 +1,5 @@
 // passwordValidator.js
 function validatePassword(password) {
-    return password.length >= 8 && /\d/.test(password);
+    return password.length >= 8 && /\d/.test(password) && /[a-zA-Z]/.test(password);
 }
 module.exports = validatePassword;
-
-
--- passwordValidator.spec.js
+++ passwordValidator.spec.js
@@ -1,13 +1,19 @@
 // passwordValidator.spec.js
 const validatePassword = require('./passwordValidator');
 
-test('at least 8 characters', () => {
+/*test('at least 8 characters', () => {
     expect(validatePassword('2345678911')).toBe(true);
     expect(validatePassword('pass123')).toBe(false);
 });
 
-// passwordValidator.spec.js
+
 test('password has at least one digit', () => {
     expect(validatePassword('password1')).toBe(true);
     expect(validatePassword('password')).toBe(false);
+})*/
+
+
+test('should validate that password has at least one letter', () => {
+    expect(validatePassword('password1')).toBe(true);
+    expect(validatePassword('12345678')).toBe(false);
 });
